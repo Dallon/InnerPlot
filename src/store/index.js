af5fdf5ca47c07aae17dfd1edcd80e5e04ba58f4
Dallon <dboultbee@hotmail.com>
2023-09-18 11:41:57 -0700

    index.js unused imports removed

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { authReducer } from './slices/authSlice';
 import { gameStateReducer } from './slices/gameStateSlice';
-import userProfileSlice from './slices/userProfileSlice';
-import gardenStateSlice from './slices/gardenStateSlice';
-import tasksSlice from './slices/tasksSlice';
+// import userProfileSlice from './slices/userProfileSlice';
+// import gardenStateSlice from './slices/gardenStateSlice';
+// import tasksSlice from './slices/tasksSlice';
 
 export const store = configureStore({
     reducer: {
