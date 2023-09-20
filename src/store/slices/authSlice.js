c233f9a2dcc54fde370d5d0d64585d938c700afb
Dallon <dboultbee@hotmail.com>
2023-09-20 16:07:19 -0700

    added a thunk to authenticate user, token to initial state, notes, and code to build out fulfilled and rejected reducer cases

--- src/store/slices/authSlice.js
+++ src/store/slices/authSlice.js
@@ -1,8 +1,30 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+
+export const authenticateUser = createAsyncThunk(
+  'auth/authenticateUser',
+  async (_, {rejectWithValue})=>{
+    try{
+      const user = firebase.auth().currentUser;
+    if (user) {
+      //retrieve token
+      const token = await user.getIdToken();
+      //return user and token
+      return { user: user.uid, token};
+    } else{
+      return rejectWithValue('No user is signed in.');
+    }
+    } catch (error){
+      return rejectWithValue(error.message);
+    }
+  }
+);
 
 const initialState = {
   isAuthenticated: false,
   userId: null,
+  token: null,
   loading: false,
   error: null,
 };
@@ -11,22 +33,38 @@ const authSlice = createSlice({
   name: 'auth',
   initialState,
   reducers: {
-    setAuthenticated: (state, action) => {
-      state.isAuthenticated = action.payload;
-      console.log(action.payload);
-    },
-    setUserId: (state, action) => {
-      state.userId = action.payload;
-      
-    },
-  },
-  extraReducers: (builder) => {
-    // Your existing extraReducers for thunks can stay here
-  },
+    //synchronous reducers
+    //In the setUserId reducer, the assumption is that the
+    //  action.payload directly contains the value forthe user ID
+
+          setAuthenticated: (state, action) => {
+            state.isAuthenticated = action.payload;
+            console.log(action.payload);
+          },
+          setUserId: (state, action) => {
+            state.userId = action.payload;
+            
+          },
+        },
+
+  extraReducers:(builder) =>{
+    builder
+    .addCase(authenticateUser.fulfilled, (state, action) => {
+      state.isAuthenticated = true;
+      state.userId = action.payload.user;
+      state.token = action.payload.token;
+      state.loading = false;
+    })
+    .addCase(authenticateUser.rejected, (state, action) => {
+      state.isAuthenticated = false;
+      state.loading = false;
+      state.error = action.payload;
+    });
+
+  }
 });
 
 export const { setAuthenticated, setUserId } = authSlice.actions;
-
 export const authReducer = authSlice.reducer;
 
 
