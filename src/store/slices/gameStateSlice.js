f9fe9d14e21a98fe75476e6f1f4b3ff0be8e0a0c
Dallon <dboultbee@hotmail.com>
2023-09-20 16:10:57 -0700

    added allIDs, byId to my initialState of objects for optimization and mutability purposes

--- src/store/slices/gameStateSlice.js
+++ src/store/slices/gameStateSlice.js
@@ -1,12 +1,19 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-    objects: [
-        //sample objects
-        {x: 0, y: 0, type: 'tree', id: '1'},
-        {x: 1, y: 0, type: 'rock', id: '2'},
-
-    ]
+  objects: {
+    //byId is like a lookup table. It allows you
+    // to quickly find and modify an object using its id.
+    byId: {
+      '1': { x: 10, y: 30, type: 'tree', id: '1' },
+      '2': { x: 15, y: 40, type: 'rock', id: '2' }
+    },
+    //allIds is an array that holds all the ids in the order they were 
+    //added or based on some other sorting criteria you may have.
+    // It is useful for scenarios where you need to know the order 
+    //of objects or iterate over them in a specific sequence.
+    allIds: ['1', '2']
+  }
 };
 
 export const gameStateSlice  = createSlice({
@@ -14,11 +21,14 @@ export const gameStateSlice  = createSlice({
     initialState,
     reducers: {
         addObject: (state, action) => {
-            state.objects.push(action.payload);
-            console.log(action.payload);
+          const newObject = action.payload;
+          state.objects.byId[newObject.id] = newObject;
+          state.objects.allIds.push(newObject.id);
         },
-        removeObject: (state, action) => { 
-            state.objects = state.objects.filter(obj => obj.id !== action.payload.id);
+        removeObject: (state, action) => {
+          const idToRemove = action.payload;
+          delete state.objects.byId[idToRemove];
+          state.objects.allIds = state.objects.allIds.filter(id => id !== idToRemove);
         },
     },
 });
