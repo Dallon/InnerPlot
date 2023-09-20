d14015a7fefb4c232abe88730fc2d390b6bff5cc
Dallon <dboultbee@hotmail.com>
2023-09-20 16:05:46 -0700

    added boxSlice reducer

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { authReducer } from './slices/authSlice';
 import { gameStateReducer } from './slices/gameStateSlice';
+import { boxSliceReducer } from './slices/boxSlice';
 // import userProfileSlice from './slices/userProfileSlice';
 // import gardenStateSlice from './slices/gardenStateSlice';
 // import tasksSlice from './slices/tasksSlice';
@@ -9,6 +10,8 @@ export const store = configureStore({
     reducer: {
         auth: authReducer,
         gameState: gameStateReducer,
+        boxState: boxSliceReducer,
+
         // userProfile: userProfileSlice.reducer,
         // gardenState: gardenStateSlice.reducer,
         // tasks: tasksSlice.reducer,
