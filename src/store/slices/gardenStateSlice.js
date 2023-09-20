b2a7e119c94dc7cffe849952d782b9e22b1867c7
Dallon <dboultbee@hotmail.com>
2023-09-20 16:12:54 -0700

    added a thunk which should fetch garden data, added data, status to initialState, and reducers to handle the data and status.

--- src/store/slices/gardenStateSlice.js
+++ src/store/slices/gardenStateSlice.js
@@ -1,25 +1,45 @@
+    // gardenStateSlice.js
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
-import { authReducer } from './authSlice';
-export const fetchGarden = createAsyncThunk('gardenState/fetchGarden',
-    async (userId) => {
-        // Taking in userId , fetch garden state from server
-        //steps here-- verify userID and valid token, fetch data
-        //
+import { fetchData } from '../thunks/fetchGardenDataThunk';
 
+// fetchData is responsible for fetching data with
+// 'gardenState/fetchData' as a unique action type string
+const fetchData = fetchData;
 
-    });
-
-const gardenStateSlice = createSlice({
-    name: 'gardenState',
-    initialState: {
-        grid: [],
-        isLoading: false,
-    },
-    extraReducers: (builder) => {
-        builder.addCase(fetchGarden.fulfilled, (state, action) => {
+// Define the initial state and reducers using createSlice from Redux Toolkit
+export const gardenStateSlice = createSlice({
+  name: 'gardenState', // The name of the slice, used as a prefix for action types
+  initialState: { // The initial state of this slice
+    data: null,
+    grid: [],
+    isLoading: false,
+    status: 'idle',
+  },
+  reducers: { 
+    // Synchronous reducers can be defined here
+  },
+  
+  extraReducers: (builder) => { 
+    // Handle actions defined outside this slice
+    // We add extra reducers to handle the cases when the fetchData thunk is fulfilled or rejected
+    builder
+      .addCase(fetchData.fulfilled, (state, action) => {
+        // When the promise is successful, update the state with the returned data
+        state.data = action.payload;
+        state.status = 'succeeded';
+      })
+      .addCase(fetchData.rejected, (state, action) => {
+        // When the promise is rejected, update the state with an error message
+        state.status = 'failed';
+        state.error = action.payload;
+      })
+       .addCase(fetchGarden.fulfilled, (state, action) => {
                 state.grid = action.payload;
                 state.isLoading = false;
             });
-
-    },
+  },
 });
+
+// Export the automatically generated reducer function
+export default gardenStateSlice.reducer;
+
