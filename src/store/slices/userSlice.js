28426febcc53639e2f0e435d7c48f8514340ca48
Dallon <dboultbee@hotmail.com>
2023-09-22 16:53:25 -0700

    added updateUserProfile thunk (probably will move this later), as well as its extra (Async) reducer cases

--- src/store/slices/userSlice.js
+++ src/store/slices/userSlice.js
@@ -1,8 +1,28 @@
-import { createSlice } from '@reduxjs/toolkit';
-import fetchUserData from '../thunks/fetchUserData';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import { fetchUserData } from '../thunks/fetchUserData';
+import { db } from '../../firebase';
 
 // User slice is used to manage additional user-specific data that is fetched 
 // from Firestore after the user has been authenticated
+
+export const updateUserProfile = createAsyncThunk(
+  'user/updateUserProfile',
+  async ({ userId, profileData }) => {
+    // No need to get userId from getState since it's passed as a parameter
+
+    // Create a reference to the user document in Firestore
+    const userRef = db.collection('users').doc(userId);
+
+    // Update the username field in the user's profile
+    await userRef.update({
+      'profile.username': profileData.name
+    });
+
+    // Return the updated profile data
+    return { username: profileData.name };
+  }
+);
+
 
 const userSlice = createSlice({
   name: 'user',
@@ -28,6 +48,14 @@ const userSlice = createSlice({
       .addCase(fetchUserData.rejected, (state, action) => {
         state.error = action.payload;
         state.loading = false;
+      }).addCase(updateUserProfile.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      }).addCase(updateUserProfile.fulfilled, (state, action) => {
+        state.userData.profile = action.payload;
+        state.loading = false;
+        state.error = null;
+
       });
   },
 });
