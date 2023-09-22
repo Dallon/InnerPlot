db4465f1c43fdfef961065d4416b5c5bf9f365a3
Dallon <dboultbee@hotmail.com>
2023-09-21 17:21:46 -0700

    added userSlice's reducer "userReducer"

--- src/store/index.js
+++ src/store/index.js
@@ -2,6 +2,7 @@ import { configureStore } from '@reduxjs/toolkit';
 import { authReducer } from './slices/authSlice';
 import { gameStateReducer } from './slices/gameStateSlice';
 import { boxSliceReducer } from './slices/boxSlice';
+import { userReducer } from './slices/userSlice';
 // import userProfileSlice from './slices/userProfileSlice';
 // import gardenStateSlice from './slices/gardenStateSlice';
 // import tasksSlice from './slices/tasksSlice';
@@ -11,6 +12,7 @@ export const store = configureStore({
         auth: authReducer,
         gameState: gameStateReducer,
         boxState: boxSliceReducer,
+        user: userReducer,
 
         // userProfile: userProfileSlice.reducer,
         // gardenState: gardenStateSlice.reducer,
