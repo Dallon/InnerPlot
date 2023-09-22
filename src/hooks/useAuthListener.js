6331063339ec83de564f58ff6d42315e3f1de8e5
Dallon <dboultbee@hotmail.com>
2023-09-21 17:56:23 -0700

    Listener checks for state change of user, dispatches authentication state and id, and then detaches the listener

---
+++ src/hooks/useAuthListener.js
@@ -0,0 +1,23 @@
+import { useEffect } from "react";
+import firebaseInstance from "../firebase";
+import { useDispatch } from "react-redux";
+import { setAuthenticated, setUserId } from "../store/slices/authSlice";
+
+const useAuthListener = () => {
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        const detachAuthListener = firebaseInstance.auth()
+        .onAuthStateChanged((user) =>{
+            if (user) {
+                dispatch(setAuthenticated(true));
+                dispatch(setUserId(user.uid));
+            } else {
+                dispatch(setAuthenticated(false));
+                dispatch(setUserId(null));
+            }
+        });
+        //detaches authStateListener when component unmounts
+        return () => detachAuthListener();
+    })
+}
