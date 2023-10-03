0afdea8d372a5fd3b92d2c78b5c891b351a25bf5
Dallon <dboultbee@hotmail.com>
2023-10-03 12:05:54 -0700

    much of the logic moved to a  hook

--- src/store/slices/authSlice.js
+++ src/store/slices/authSlice.js
@@ -1,29 +1,10 @@
-import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
-import firebase from 'firebase/compat/app';
+import { createSlice } from '@reduxjs/toolkit';
 import 'firebase/compat/auth';
 
-export const authenticateUser = createAsyncThunk(
-  'auth/authenticateUser',
-  async (_, {rejectWithValue})=>{
-    try{
-      const user = firebase.auth().currentUser;
-    if (user) {
-   
-      return { user: user.uid};
-    } else{
-      return rejectWithValue('No user is signed in.');
-    }
-    } catch (error){
-      return rejectWithValue(error.message);
-    }
-  }
-);
 
 const initialState = {
   isAuthenticated: false,
   userId: null,
-  loading: false,
-  error: null,
 };
 
 const authSlice = createSlice({
@@ -31,34 +12,19 @@ const authSlice = createSlice({
   initialState,
   reducers: {
     //synchronous reducers
-    //In the setUserId reducer, the assumption is that the
-    //  action.payload directly contains the value for the user ID
+    //In the setUserId reducer, the assumption is that the action.payload 
+    //directly contains the value for the user ID, handled by authReducer hook
 
           setAuthenticated: (state, action) => {
             state.isAuthenticated = action.payload;
-            console.log(action.payload);
+            // console.log(action.payload);
           },
           setUserId: (state, action) => {
             state.userId = action.payload;
-            console.log("User ID: " + action.payload)
+            // console.log("User ID: " + action.payload)
             
           },
         },
-
-  extraReducers:(builder) =>{
-    builder
-    .addCase(authenticateUser.fulfilled, (state, action) => {
-      state.isAuthenticated = true;
-      state.userId = action.payload;
-      state.loading = false;
-    })
-    .addCase(authenticateUser.rejected, (state, action) => {
-      state.isAuthenticated = false;
-      state.loading = false;
-      state.error = action.payload;
-    });
-
-  }
 });
 
 export const { setAuthenticated, setUserId } = authSlice.actions;
