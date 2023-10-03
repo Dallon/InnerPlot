be2a80bf5302ba0c3676485693c097fe9816d3b0
Dallon <dboultbee@hotmail.com>
2023-10-03 12:13:49 -0700

    added "handleAvatarChange" event handler, needs work

--- src/components/ui/UserProfileComponent.js
+++ src/components/ui/UserProfileComponent.js
@@ -1,45 +1,66 @@
 import React, { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { updateUserProfile } from '../../store/slices/userSlice';
-import { auth } from 'firebaseui';
 
 const UserProfile = () => {
   // Get the user data from the Redux store
   const userData = useSelector((state) => state.user.userData);
-  const userId = useSelector ((state) => state.auth.userId);
-  const wholeState = useSelector((state) => state);
-  
-  // Log the entire user data and Redux state to console
-  console.log('User Data:', userData);
-  console.log('Whole Redux State:', wholeState);
+  const userId = useSelector((state) => state.auth.userId);
 
   const dispatch = useDispatch();
-  
-  console.log(userData);
-  
-  // Local state for form input
-  //check first if userData exists.
+  //State for form input, checking first if userData exists
   const [name, setName] = useState(userData?.profile.username || 'defaultName');
-  
+  const [avatar, setAvatar] = useState(userData?.profile.avatar || null);
+
+
+  const handleAvatarChange = (event) => {
+    event.preventDefault();
+
+    // Retrieve the file from the event object
+    const file = event.target.avatar.files[0];
+
+    if (file) {
+      // temorarily using a local URL for the file object.
+      const avatarUrl = URL.createObjectURL(file);
+      setAvatar(avatarUrl);
+      dispatch(updateUserProfile({ userId: userId, profileData: { avatar: avatarUrl } }));
+    }
+  }
+
   // Handle form submission
   const handleSubmit = (event) => {
     event.preventDefault();
- 
+
     // Dispatch the thunk to update the user profile in Firestore
     dispatch(updateUserProfile({ userId: userId, profileData: { name } }));
-       console.log('User Data at handleSubmit:', userData);
-    console.log("did we get here");
-    console.log("the name once it is set is: " + name )
+    console.log("the name once it is set is: " + name)
   };
-  
+
   return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        Name:
-        <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
-      </label>
-      <button type="submit">Save Changes</button>
-    </form>
+    <div>
+      <div>
+        <form onSubmit={handleSubmit}>
+          <label>
+            Name:
+            <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+          </label>
+          <button type="submit">Save Changes</button>
+        </form>
+      </div>
+      <div>
+      <div>
+        <form onSubmit={handleAvatarChange}>
+          <label>
+            Avatar:
+            <input type="file" name="avatar" />
+          </label>
+          <button type="submit">Upload Avatar</button>
+        </form>
+        {/* If 'avatar' is falsey /does not have a meaningful value, this img tag will not be rendered.*/}
+        {avatar && <img src={avatar} alt="User Avatar" />}
+      </div>
+      </div>
+    </div>
   );
 };
 
