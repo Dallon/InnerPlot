e895954ff996dfe8a5f9c1904154168cc1e7d696
Dallon <dboultbee@hotmail.com>
2023-10-07 10:15:52 -0700

    implement createSelector, deprecated replacement line 34

--- src/hooks/useLoadItems.js
+++ src/hooks/useLoadItems.js
@@ -4,38 +4,43 @@ import * as PIXI from 'pixi.js';
 import { useSelector, useDispatch } from 'react-redux';
 import { loadSVGAsTexture } from '../utils/loadSVGAsTexture';
 import { addInventoryItem, removeObject } from '../store/slices/gameStateSlice';
+import { createSelector } from 'reselect';
+
+const selectObjectsById = createSelector(
+  state => state.gameState.objects.byId,
+  byId => Object.values(byId),
+);
 
 export const useLoadItems = (containerRef) => {
   const dispatch = useDispatch();
-  const itemsFromState = useSelector(state => Object.values(state.gameState.objects.byId));
+  const itemsFromState = useSelector(selectObjectsById);
 
   useEffect(() => {
+    containerRef.current.removeChildren();
     itemsFromState.forEach(async item => {
       // Convert Cartesian coordinates to isometric
       const isoX = (item.x - item.y) / 2;
       const isoY = (item.x + item.y) / 2;
+      console.log("Is it duplicating?")
 
       try {
-        //load each item as a PIXI texture
         const texture = await loadSVGAsTexture(item.url);
-        //create a pixi sprite from the texture
         const itemSprite = new PIXI.Sprite(texture);
-        //Set the isometric position
         itemSprite.x = isoX;
         itemSprite.y = isoY;
-        // Add click event to sprite (temporarily here)
+        itemSprite.name = item.id;
 
-        itemSprite.interactive = true;
+        // Event Logic
+        itemSprite.eventMode = 'dynamic';
         itemSprite.buttonMode = true;
         itemSprite.on('pointerdown', () => {
           dispatch(addInventoryItem({ id: item.id, type: item.type }));
-          console.log("Dispatching removeObject with id:", item.id);
           dispatch(removeObject(item.id));
-          console.log(itemSprite + "added to invent");
         });
 
-        //Add sprite to container
+        // Add sprite to container
         containerRef.current.addChild(itemSprite);
+        
       } catch (error) {
         console.error('Error in loading texture:', error);
       }
