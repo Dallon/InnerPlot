f4508062aa9c8b6998d4d51cde76e5028e38b86f
Dallon <dboultbee@hotmail.com>
2023-10-11 15:05:22 -0700

    create hook to set up inventory container

---
+++ src/hooks/useInventoryContainer.js
@@ -0,0 +1,53 @@
+// useCreateInventoryContainer.js
+import { useEffect } from 'react';
+import * as PIXI from 'pixi.js';
+import { useSelector } from 'react-redux';
+
+export const useInventoryContainer = (appRef, inventoryContainerRef) => {
+    const inventoryOpen = useSelector(state => state.ui.isInventoryOpen);
+
+    useEffect(() => {
+        console.log(appRef.current.stage.children);
+        if (!appRef.current) {
+            console.error("App is not initialized.");
+            return;
+        }
+
+        // Set Dimensions
+       
+        inventoryContainerRef.current.name = 'inventory';
+
+     
+        // set background color for inventory
+        const graphics = new PIXI.Graphics();
+        graphics.beginFill(0xFFFFFF, 0.2); // Solid red for clear visibility
+        graphics.drawRect(0, 0, 500, 300);
+        graphics.endFill();
+        inventoryContainerRef.current.addChild(graphics);
+        graphics.interactive = true;
+        graphics.on('pointerdown', () => {
+            console.log("Inventory clicked!");
+        });
+
+        // Position the Container at the bottom center of the app screen:
+        inventoryContainerRef.current.x = (appRef.current.screen.width / 2) - (inventoryContainerRef.current.width / 2);
+        inventoryContainerRef.current.y = appRef.current.screen.height - inventoryContainerRef.current.height - 10; // 10px padding from bottom
+
+
+
+        // Add/Remove the inventoryContainerRef to/from stage
+        if (inventoryOpen) {
+            console.log("test invent open in useInventoryContainer");
+            if (!appRef.current.stage.children.includes(inventoryContainerRef.current)) {
+                appRef.current.stage.addChild(inventoryContainerRef.current);
+                console.log(inventoryContainerRef.current.visible, inventoryContainerRef.current.alpha);
+
+            }
+        } else {
+            console.log("test invent closed in useInventoryContainer");
+            if (appRef.current.stage.children.includes(inventoryContainerRef.current)) {
+                appRef.current.stage.removeChild(inventoryContainerRef.current);
+            }
+        }
+    }, [ inventoryOpen]);
+};
