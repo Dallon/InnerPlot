81a692ace27c060f97fce2704d0ce71523513ed0
Dallon <dboultbee@hotmail.com>
2023-10-11 15:12:32 -0700

    Created AssetManifest to organize png imports in PixiJS fashion

---
+++ src/AssetManifest.js
@@ -0,0 +1,19 @@
+// Import the images
+import blackOakSeedling from './assets/images/blackOakSeedling.png';
+import bluePotion from './assets/images/bluePotion.png';
+import greenPotion from './assets/images/greenPotion.png';
+import grass from './assets/images/grass.png';
+
+//use images in manifest
+export const AssetManifest = {
+    images: {
+        items: {
+            blackOakSeedling: blackOakSeedling,
+            bluePotion: bluePotion,
+            greenPotion: greenPotion
+        },
+        textures: {
+            grass: grass
+        }
+    }
+};
