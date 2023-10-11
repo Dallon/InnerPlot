72090471dd8579b74b959bc9c67a79a4aa845692
Dallon <dboultbee@hotmail.com>
2023-10-11 15:08:49 -0700

    grid steup to use png repeating texture

--- src/hooks/useCreateIsoGrid.js
+++ src/hooks/useCreateIsoGrid.js
@@ -1,9 +1,27 @@
 import { IsoGrid } from "../utils/IsoGridUtil";
 import { useEffect } from "react";
+import { useState, useCallback } from 'react';
+import { useLoadPNG } from '../hooks/useLoadPNG';
 
 // This is the useEffect to render the isometric tiles using the texture
-export const useCreateIsoGrid = (gridContainerRef, texture, tileWidth, tileHeight) => {
+export const useCreateIsoGrid = (gridContainerRef) => {
 
+
+    const [texture, setTexture] = useState(null);
+
+    // Define the tile width and height
+    const tileWidth = 64; 
+    const tileHeight = 64; 
+
+    //declaring the svgURL and the useLoadSVG hook outside the useEffect below
+    const pngURL = process.env.PUBLIC_URL + '/squareIsoTest4.png';
+
+        const handleTextureLoaded = useCallback((newlyLoadedTexture) => {
+        setTexture(newlyLoadedTexture); 
+    }, []);
+
+
+    const loadedTexture = useLoadPNG(pngURL, handleTextureLoaded);
     useEffect(() => {
         if (texture) {
 
