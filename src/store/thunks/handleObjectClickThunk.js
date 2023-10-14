bfc6237deb87d6a37970691c4964ee521433c5fa
D <dallonblt@gmail.com>
2023-10-13 17:14:08 -0700

    parameter change

--- src/store/thunks/handleObjectClickThunk.js
+++ src/store/thunks/handleObjectClickThunk.js
@@ -1,18 +1,18 @@
 import { addInventoryItem } from "../slices/inventorySlice";
 import { removeObject } from "../slices/gameStateSlice";
 
-// This is a thunk action creator.
-export const handleObjectClick = (objectId) => {
+// This thunk action creator 
+export const handleObjectClick = (item) => {
     //dispatch is provided by the redux store.
     //because we use a thunk we access the middleware dispatch without
     //importing it.
     return dispatch => {
         // Directly add the object to the inventory.
         dispatch(addInventoryItem({
-            id: objectId
+            id: item.id
         }));
 
         // Then, remove object from game area
-        dispatch(removeObject({ id: objectId }));
+        dispatch(removeObject({ id: item.id }));
     };
 }
