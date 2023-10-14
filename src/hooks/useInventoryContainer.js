a8954666f2874b7d04eaee016bf90f61f1864be8
D <dallonblt@gmail.com>
2023-10-13 17:16:48 -0700

    fixed.. a silly mistake of adding a new child to the container every time I wanted to see the inventory, instead of just doing one initial load and then setting it as interactible and visible.

--- src/hooks/useInventoryContainer.js
+++ src/hooks/useInventoryContainer.js
@@ -3,51 +3,38 @@ import { useEffect } from 'react';
 import * as PIXI from 'pixi.js';
 import { useSelector } from 'react-redux';
 
-export const useInventoryContainer = (appRef, inventoryContainerRef) => {
-    const inventoryOpen = useSelector(state => state.inventory.isInventoryOpen);
 
-    useEffect(() => {
-        console.log(appRef.current.stage.children);
-        if (!appRef.current) {
-            console.error("App is not initialized.");
-            return;
-        }
+ let initialized = false;
 
-        // Set Dimensions
-       
-        inventoryContainerRef.current.name = 'inventoryContainer';
-
-     
-        // set background color for inventory. Here this gets loaded every time we click the inventory which is bad.
-        const graphics = new PIXI.Graphics();
-        graphics.beginFill(0xFFFFFF, 0.2); // Solid red for clear visibility
-        graphics.drawRect(0, 0, 500, 300);
-        graphics.endFill();
-        inventoryContainerRef.current.addChild(graphics);
-        graphics.interactive = true;
-        graphics.on('pointerdown', () => {
-            console.log("Inventory clicked!");
-        });
-
-        // Position the Container at the bottom center of the app screen:
-        inventoryContainerRef.current.x = (appRef.current.screen.width / 2) - (inventoryContainerRef.current.width / 2);
-        inventoryContainerRef.current.y = appRef.current.screen.height - inventoryContainerRef.current.height - 10; // 10px padding from bottom
-
-
-
-        // Add/Remove the inventoryContainerRef to/from stage
-        if (inventoryOpen) {
-            console.log("test invent open in useInventoryContainer");
-            if (!appRef.current.stage.children.includes(inventoryContainerRef.current)) {
-                appRef.current.stage.addChild(inventoryContainerRef.current);
-                console.log(inventoryContainerRef.current.visible, inventoryContainerRef.current.alpha);
-
-            }
-        } else {
-            console.log("test invent closed in useInventoryContainer");
-            if (appRef.current.stage.children.includes(inventoryContainerRef.current)) {
-                appRef.current.stage.removeChild(inventoryContainerRef.current);
-            }
-        }
-    }, [ inventoryOpen]);
-};
+ export const useInventoryContainer = (appRef, inventoryContainerRef) => {
+     const inventoryOpen = useSelector(state => state.inventory.isInventoryOpen);
+ 
+     useEffect(() => {
+         if (!appRef.current) {
+             console.error("App is not initialized.");
+             return;
+         }
+ 
+         if (!initialized) {
+             initialized = true;
+ 
+             // Create background
+             const graphics = new PIXI.Graphics();
+             graphics.beginFill(0xFFFFFF, 0.2);
+             graphics.drawRect(0, 0, 500, 300);
+             graphics.endFill();
+             inventoryContainerRef.current.addChild(graphics);
+             
+             // Initialize your grid and other components here.
+ 
+             // Add to stage only once.
+             appRef.current.stage.addChild(inventoryContainerRef.current);
+         }
+ 
+         // Toggle visibility and interactivity
+         inventoryContainerRef.current.visible = inventoryOpen;
+         inventoryContainerRef.current.interactive = inventoryOpen;
+ 
+     }, [inventoryOpen]);
+ };
+ 
