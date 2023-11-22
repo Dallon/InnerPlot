b6a54a02554d430f48b3695116d1d2f4c1fba4ad
Dallon <dboultbee@hotmail.com>
2023-11-21 19:34:37 -0800

    comments explain the changes

--- src/hooks/useContainerConnections.js
+++ src/hooks/useContainerConnections.js
@@ -1,18 +1,40 @@
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import * as PIXI from 'pixi.js';
 import { useViewport } from '../hooks/useViewport';
 import { useInventoryContainer } from '../hooks/useInventoryContainer'; // Import the hook
+import { useCreateIsoGrid } from "./useCreateIsoGrid";
+import { useCreateInteractiveGrid } from "./useCreateInteractiveGrid";
+import { useContext } from "react";
+import AppContext from "../contexts/AppContext";
 
-export const useContainerConnections = (appRef, stageRef, mainContainerRef, gridContainerRef, objectsContainerRef, inventoryContainerRef) => {
-    // First, set up the inventory container
-    useInventoryContainer(appRef, inventoryContainerRef); 
+export const useContainerConnections = (mainContainerRef, objectsContainerRef, texture) => {
+    
+    const { appRef, inventoryContainerRef } = useContext(AppContext);
+
+    // Initialize stageRef with a new PIXI Container so it can be used in useEffects
+    const stageRef = useRef(new PIXI.Container());
+    const UIContainerRef = useRef(new PIXI.Container());
+   
+    //get the gridContainerRef
+    const gridContainerRef = useRef(new PIXI.Container());
+    gridContainerRef.name = 'gridContainer';
+
+    const gameContainerRef = useRef(new PIXI.Container());
+    // This is the useEffect to render the isometric tiles using the loaded texture
+    useCreateIsoGrid(gridContainerRef, texture);
+
+    //the interactive grid goes on top of the isometric grid
+    const interactiveGridContainerRef = useRef(new PIXI.Container());
+    useCreateInteractiveGrid(interactiveGridContainerRef)
 
     const viewportRef = useViewport(appRef, stageRef);
 
     useEffect(() => {
+
+        console.log(`container connections hook is running`);//debugging
         // Check for all null references before proceeding
-        const refsToCheck = [viewportRef.current, stageRef.current, appRef.current.stage, gridContainerRef.current, objectsContainerRef.current];
-        const refNames = ['viewportRef', 'stageRef', 'appRef.stage'];
+        const refsToCheck = [viewportRef.current, stageRef.current, appRef.current, gridContainerRef.current, objectsContainerRef.current];
+        const refNames = ['viewportRef', 'stageRef', 'appRef.current', 'gridContainerRef', 'objectsContainerRef'];
 
         refsToCheck.forEach((ref, index) => {
             if (!ref) console.error(`${refNames[index]}.current is not initialized`);
@@ -21,22 +43,25 @@ export const useContainerConnections = (appRef, stageRef, mainContainerRef, grid
         // If all checks pass, then add viewportRef to the app's stage
         appRef.current.stage.addChild(viewportRef.current);
         appRef.current.stage.addChild(inventoryContainerRef.current);
-        appRef.current.stage.sortableChildren = true;
-
 
         //move this to a new independent file
         const inventorySprite = PIXI.Sprite.from(process.env.PUBLIC_URL + '/InventoryBag.svg');
         inventorySprite.y = appRef.current.screen.height;
-        console.log("inventorySpriteY:" + inventorySprite.y);
 
         // Add mainContainer to the viewport
         viewportRef.current.addChild(mainContainerRef.current);
 
         //z index specified to ensure loading order of objects on top
-        mainContainerRef.current.addChildAt(gridContainerRef.current, 0);
-        mainContainerRef.current.addChildAt(objectsContainerRef.current, 1);
+        mainContainerRef.current.addChildAt(gameContainerRef.current, 0);
+        mainContainerRef.current.addChildAt(UIContainerRef.current, 1);
+        gameContainerRef.current.addChildAt(gridContainerRef.current, 0);
+        gameContainerRef.current.addChildAt(interactiveGridContainerRef.current, 1);
+        gameContainerRef.current.addChildAt(objectsContainerRef.current, 2);
+        gameContainerRef.current.sortableChildren = true;
+
         objectsContainerRef.current.name = 'ground';
-    
+
+
     }, []);
 
 };
