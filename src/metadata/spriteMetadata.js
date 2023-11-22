976a5fb41c25c228e06e2662d73e8c0fc07831e7
Dallon <dboultbee@hotmail.com>
2023-11-21 19:59:16 -0800

    future location of sprite metadata

---
+++ src/metadata/spriteMetadata.js
@@ -0,0 +1,16 @@
+// SpriteMetadata.js
+export const SpriteMetadata = {
+    bush1Sprite: {
+      initialAnimationSpeed: 0.01,
+      // ...other properties
+    },
+    flower1Sprite: {
+      initialAnimationSpeed: 0.005,
+      
+      // ...other properties
+    },
+    soil1Sprite: {
+                initialAnimationSpeed: 0.1,
+            },
+  };
+  
