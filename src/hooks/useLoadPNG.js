a6832069f1770fc798ab52e47aefce205aa521dc
Dallon <dboultbee@hotmail.com>
2023-11-21 19:45:15 -0800

    clarified what the function is used for, console.log added after texture loaded not before.

--- src/hooks/useLoadPNG.js
+++ src/hooks/useLoadPNG.js
@@ -1,17 +1,20 @@
 import { useEffect } from 'react';
-import { loadPNGAsTexture } from '../utils/loadPNGAsTexture';
 
+import { loadPNGAsTexture } from '../utils/loadPNGAsTexture';
+//used in the background grid creation process, not item creation process
 export const useLoadPNG = (url, onTextureLoaded) => {
+
     useEffect(() => {
         let isMounted = true;
         console.log('useLoadPNG effect started', { url });
-
+        
         loadPNGAsTexture(url)
             .then(texture => {
                 if (!isMounted) return;
-                console.log("texture loaded");
+                
                 if (onTextureLoaded) {
                     onTextureLoaded(texture);
+                    console.log("texture loaded");
                   }
             })
             .catch(error => console.error('Error loading PNG:', error));
@@ -22,3 +25,4 @@ export const useLoadPNG = (url, onTextureLoaded) => {
     }, [url]);
 
 };
+
