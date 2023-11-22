c6d0a0763ae7a48fbfb427d35a755373bf18db2e
Dallon <dboultbee@hotmail.com>
2023-11-21 19:26:04 -0800

    set the appRef to be created in the app.js to avoid re renders in PixiCanvasComponent, same with the loading of the background texture. isAppReady must be true for the components to load

--- src/App.js
+++ src/App.js
@@ -2,16 +2,30 @@ import './App.css';
 import LoginComponent from './components/ui/LoginComponent';
 import PixiCanvasComponent from './components/PixiCanvasComponent';
 import UserProfileComponent from './components/ui/UserProfileComponent';
+import InventoryComponent from './components/ui/InventoryComponent';
 import useAuthListener from './hooks/useAuthListener';
-import { useEffect } from 'react';
+import { useEffect, useRef, useState } from 'react';
+import AppContext from './contexts/AppContext';
+import { usePixiApp } from './hooks/usePixiApp';
+import { useLoadPNG } from './hooks/useLoadPNG';
+import * as PIXI from 'pixi.js';
+import { AssetManifest } from './AssetManifest';
 
 function App() {
   useAuthListener();
 
- 
   const disableScroll = (event) => {
     event.preventDefault();
   };
+
+  const { appRef, isAppReady } = usePixiApp();
+  const inventoryContainerRef = useRef(new PIXI.Container());
+
+  const [texture, setTexture] = useState(null);
+  const pngURL = AssetManifest.images.textures.grass;
+
+  // Call the useLoadPNG hook here
+  useLoadPNG(pngURL, setTexture);
 
   useEffect(() => {
     // Attach the event listener
@@ -25,14 +39,17 @@ function App() {
 
 
   return (
-    <div className="App">
-      <header >
-         </header> 
-      <LoginComponent/>
-      <PixiCanvasComponent/>
-      <UserProfileComponent/>
-    </div>
+    <AppContext.Provider value={{ appRef, inventoryContainerRef }}>
+      <div className="App">
+        <header >
+        </header>
+        <LoginComponent />
+        {isAppReady && <PixiCanvasComponent texture={texture}  />}
+        {isAppReady && <InventoryComponent />}
+        {isAppReady && <UserProfileComponent />}
+      </div>
+    </AppContext.Provider>
   );
-}
+};
 
 export default App;
