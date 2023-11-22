5a837bd214a347c8b9c9ac8faaf98b213c0e3308
Dallon <dboultbee@hotmail.com>
2023-11-21 19:33:26 -0800

    console.log

--- src/hooks/useCenterContainers.js
+++ src/hooks/useCenterContainers.js
@@ -1,6 +1,9 @@
 import { useEffect } from 'react';
 
 export const useCenterContainers = (mainContainerRef, gridContainerRef) => {
+
+  console.log(`center containers Hook is running`);//debugging
+
   useEffect(() => {
     // Centering logic here-- the main container will expand to hold the grid right? work on this.
     const containerCenterX = mainContainerRef.current.width;
