aa6a124dae183893008c46ae5ca3071a7fe58c4b
Dallon <dboultbee@hotmail.com>
2023-11-21 19:41:10 -0800

    the texture loading process is moved outside

--- src/hooks/useCreateIsoGrid.js
+++ src/hooks/useCreateIsoGrid.js
@@ -5,33 +5,30 @@ import { useLoadPNG } from '../hooks/useLoadPNG';
 import { AssetManifest } from "../AssetManifest";
 
 // This is the useEffect to render the isometric tiles using the texture
-export const useCreateIsoGrid = (gridContainerRef) => {
+export const useCreateIsoGrid = (gridContainerRef, texture) => {
 
 
-    const [texture, setTexture] = useState(null);
+    
 
     // Define the tile width and height
     const tileWidth = 64; 
     const tileHeight = 64; 
 
-    //declaring the svgURL and the useLoadSVG hook outside the useEffect below
-    const pngURL = AssetManifest.images.textures.grass;
 
-        const handleTextureLoaded = useCallback((newlyLoadedTexture) => {
-        setTexture(newlyLoadedTexture); 
-    }, []);
-
-
-    const loadedTexture = useLoadPNG(pngURL, handleTextureLoaded);
     useEffect(() => {
+        console.log(`useCreateIsoGrid is running`);//debugging
+        let isMounted = true; // Flag to track mounting
         if (texture) {
 
-            const rows = 50;
-            const cols = 50;
+            const rows = 20;
+            const cols = 20;
 
             const myIsoGrid = new IsoGrid(rows, cols);  // example, 10 rows and 10 cols
             myIsoGrid.renderTiles(gridContainerRef.current, texture, tileWidth, tileHeight);
         }
+        return () => {
+            isMounted = false; // Set the flag to false when the component unmounts
+        };
     }, [texture]);
 
 }
